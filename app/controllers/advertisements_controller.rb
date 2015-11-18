b78e1ed09b5f1c85d4d9c082bf2f958e31cbd131
Timothy Sorensen <tim2ade43@gmail.com>
2015-11-18 13:52:07 -0600

    Assignment 33 completed

---
+++ app/controllers/advertisements_controller.rb
@@ -0,0 +1,9 @@
+class AdvertisementsController < ApplicationController
+  def index
+  	@advertisement = Advertisement.all
+  end
+
+  def show
+  	@advertisement = Advertisement.find(params[:id])
+  end
+end
